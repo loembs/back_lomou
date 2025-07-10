76c89c96f43944a4d0febec3ec80dce5111a023d
loembs <152695197+loembs@users.noreply.github.com>
2025-07-10 18:11:33 +0000

    update logic cors

--- src/main/java/ism/atelier/atelier/config/WebConfig.java
+++ src/main/java/ism/atelier/atelier/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
             .allowedOrigins(
                 "http://localhost:8080",
                 "https://https://menu-001.vercel.app",
